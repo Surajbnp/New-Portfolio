635ed16c07455f472f09303b978a8d36a425ed31
Suraj Kumar Gupta <surajguptabnp14c@gmail.com>
2024-01-03 21:12:37 +0530

    changed

--- src/component/Experience.js
+++ src/component/Experience.js
@@ -6,10 +6,10 @@ const experience = [
     company: "Masai Labs, Bengaluru",
   },
   {
-    role: "Full Stack Developer Intern (Full-Time)",
-    from: "January, 2023",
-    to: "March, 2023",
-    company: "Masai Labs, Bengaluru",
+    role: "Web Developer Intern (Full-Time)",
+    from: "December, 2023",
+    to: "January, 2024",
+    company: "Maayasthra, Chennai, Tamil Nadu",
   },
 ];
 
